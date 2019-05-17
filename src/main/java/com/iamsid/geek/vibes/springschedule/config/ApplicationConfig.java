979baae5db40a236dda730d9db72262292e032a0
iamsid <sidhanta.ku.samantaray@gmail.com>
2019-05-17 01:01:37 -0700

    Basic Swagger Configuration

--- src/main/java/com/iamsid/geek/vibes/springschedule/config/ApplicationConfig.java
+++ src/main/java/com/iamsid/geek/vibes/springschedule/config/ApplicationConfig.java
@@ -7,7 +7,14 @@ import org.springframework.context.annotation.Configuration;
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @Configuration
+@EnableSwagger2
 public class ApplicationConfig {
 	
 	@Bean
@@ -19,4 +26,13 @@ public class ApplicationConfig {
 		scheduler.setRemoveOnCancelPolicy(true);
 		return scheduler;
 	}
+	
+    @Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.basePackage("com.iamsid.geek.vibes.springschedule.controller"))
+          .paths(PathSelectors.any())                    
+          .build();                                           
+    }
 }
